85706feb21a3bc03a89c1d7b121ad8ed04824a00
tews <tews>
2011-04-18 07:20:29 +0000

    * GPL 3

--- main.ml
+++ main.ml
@@ -3,19 +3,25 @@
  * 
  * Copyright (C) 2011 Hendrik Tews
  * 
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License as
- * published by the Free Software Foundation; either version 2 of
- * the License, or (at your option) any later version.
+ * This file is part of "prooftree".
  * 
- * This program is distributed in the hope that it will be useful,
+ * "prooftree" is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * 
+ * "prooftree" is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * General Public License in file COPYING in this or one of the
- * parent directories for more details.
+ * General Public License in file COPYING in this or one of the parent
+ * directories for more details.
  * 
- * $Id: main.ml,v 1.5 2011/04/13 10:47:08 tews Exp $
+ * You should have received a copy of the GNU General Public License
+ * along with "prooftree". If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * $Id: main.ml,v 1.6 2011/04/18 07:20:30 tews Exp $
  *)
+
 
 
 (** Main --- Argument parsing and start *)
--- proof_tree.ml
+++ proof_tree.ml
@@ -3,18 +3,23 @@
  * 
  * Copyright (C) 2011 Hendrik Tews
  * 
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License as
- * published by the Free Software Foundation; either version 2 of
- * the License, or (at your option) any later version.
+ * This file is part of "prooftree".
  * 
- * This program is distributed in the hope that it will be useful,
+ * "prooftree" is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * 
+ * "prooftree" is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * General Public License in file COPYING in this or one of the
- * parent directories for more details.
+ * General Public License in file COPYING in this or one of the parent
+ * directories for more details.
  * 
- * $Id: proof_tree.ml,v 1.7 2011/04/15 19:32:24 tews Exp $
+ * You should have received a copy of the GNU General Public License
+ * along with "prooftree". If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * $Id: proof_tree.ml,v 1.8 2011/04/18 07:20:30 tews Exp $
  *)
 
 
